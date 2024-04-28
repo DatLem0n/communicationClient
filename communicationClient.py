e9c8b3cd2916dff81a8fdf8445a16a407c887123
lem0n <sahki232@gmail.com>
2024-04-28 17:50:18 +0300

    added comments and socket timeouts

--- communicationClient.py
+++ communicationClient.py
@@ -1,66 +1,75 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import math
-import os
 import secrets
 import struct
-# The modules required
 import sys
 import socket
 
-'''
-This is a template that can be used in order to get started. 
-It takes 3 commandline arguments and calls function send_and_receive_tcp.
-in haapa7 you can execute this file with the command: 
-python3 CourseWorkTemplate.py <ip> <port> <message> 
+"""
+CommunicationClient.py
+written by Ville Kujala (ville.kujala@student.oulu.fi) on 28.04.2024 for the Introduction to Internet Coursework
+Includes all optional parts (Encryption, Multipart messages and Parity)
 
-Functions send_and_receive_tcp contains some comments.
-If you implement what the comments ask for you should be able to create 
-a functioning TCP part of the course work with little hassle.  
+Starts with a TCP connection to exchange necessary information to start UDP communication with the server.
+in UDP receives and translates messages until EOM bit is set by the server.
 
-'''
-BUF_LEN = 2048
-KEY_AMOUNT = 20
-MSG_LEN = 64
+Takes in arguments for server address, port and message, defaults are set in Main for the course server
+"""
+
+BUF_LEN = 2048      # Length of read buffer
+KEY_AMOUNT = 20     # Amount of keys used in encryption
+MSG_LEN = 64        # Length of each message block (for multipart messages)
+USAGE = 'usage: \"<server address>\" <server port> \"<message>\"'
+
 enc_keys = []
 dec_keys = []
 enc_keyNum = 0
 dec_keyNum = 0
 
 
-def send_and_receive_tcp(address, port, message):
-    print("You gave arguments: {} {} {}".format(address, port, message))
-    # create TCP socket
-    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # connect socket to given address and port
-    soc.connect((address, port))
-    # python3 sendall() requires bytes like object. encode the message with str.encode() command
-    enc = message.encode()
-    # send given message to socket
-    soc.sendall(enc)
-    # receive data from socket
-    rxBuf = soc.recv(BUF_LEN)
-    # data you received is in bytes format. turn it to string with .decode() command
-    rx_decoded = rxBuf.decode()
-    # Get your CID and UDP port from the message
-    rec_message, CID, rest = rx_decoded.split(' ')
-    port, keys = rest.split("\r\n", 1)
-    # print received data
-    print(rec_message)
+def send_and_receive_tcp(address: str, port: int, message: str) -> None:
+    """
+    responsible for exchanging necessary data via TCP to start UDP communication with the server
 
-    get_dec_keys(rx_decoded, KEY_AMOUNT)
+    :param address: address of the server
+    :param port: server port
+    :param message: initial message to the server
+    """
+    try:
+        print("You gave arguments: {} {} {}".format(address, port, message))
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        soc.settimeout(3)
+        soc.connect((address, port))
+        enc = message.encode()
+        soc.sendall(enc)
 
-    # close the socket
-    soc.close()
-    # Continue to UDP messaging. You might want to give the function some other parameters like the above mentioned cid and port.
-    send_and_receive_udp(address, port, CID)
-    return
+        rxBuf = soc.recv(BUF_LEN)
+        rx_decoded = rxBuf.decode()
+        rec_message, CID, rest = rx_decoded.split(' ')
+        port, keys = rest.split("\r\n", 1)
+        print(rec_message)
+        soc.close()
+
+        get_dec_keys(rx_decoded, KEY_AMOUNT)
+
+        send_and_receive_udp(address, port, CID)
+        return
+    except TimeoutError:
+        print("could not connect to TCP server")
+        sys.exit(USAGE)
 
 
-def encrypt(message):
+
+def encrypt(message: str) -> str:
+    """
+    encrypts the message with enc_keys
+    :param message:
+    :return: encrypted message
+    """
     new = ""
     global enc_keyNum
-    if (enc_keyNum < KEY_AMOUNT):
+    if enc_keyNum < KEY_AMOUNT:
         key = enc_keys[enc_keyNum]
         for i in range(len(message)):
             new += chr((ord(message[i]) ^ ord(key[i])))
@@ -70,10 +79,15 @@ def encrypt(message):
     return new
 
 
-def decrypt(message):
+def decrypt(message: str) -> str:
+    """
+    decrypts the message with dec_keys
+    :param message:
+    :return: decrypted message
+    """
     new = ""
     global dec_keyNum
-    if (dec_keyNum < KEY_AMOUNT):
+    if dec_keyNum < KEY_AMOUNT:
         key = dec_keys[dec_keyNum]
         for i in range(len(message)):
             new += chr((ord(message[i]) ^ ord(key[i])))
@@ -83,42 +97,63 @@ def decrypt(message):
     return new
 
 
-def get_dec_keys(message, keyAmount):
+def get_dec_keys(message: str, keyAmount: int) -> None:
+    """
+    gets decryption keys from message and inserts them into dec_keys
+    :param message:
+    :param keyAmount: amount of decryption keys to read
+    """
     global dec_keys
     dec_keys = message.split("\r\n")[1:keyAmount + 1]
 
 
-def gen_enc_keys(keyAmount):
+def gen_enc_keys(keyAmount: int) -> None:
+    """
+    generates 64 byte hex strings to use in encryption and adds them to enc_keys
+    :param keyAmount: amount of keys to generate
+    """
     for i in range(0, keyAmount):
         enc_keys.append(secrets.token_hex(32))
 
 
-def send_and_receive_udp(address, port, CID):
-    '''
-    Implement UDP part here.
-    '''
-    ACK = True
-    EOM = False
-    REMAIN = 0
-    soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    message = "Hello from {}".format(CID)
-    sendUDP(ACK, CID, EOM, REMAIN, message, address, port, soc)
+def send_and_receive_udp(address: str, port: int, CID: str) -> None:
+    """
+    handles UDP communication with the server until EOM bit is set by the server
+    :param address: server address
+    :param port: server port
+    :param CID: Unique identifier for the client given by the server in TCP
+    """
+    try:
+        ACK = True
+        EOM = False
+        soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        soc.settimeout(3)
+        message = f"Hello from {CID}"
 
-    while not EOM:
-        ACK, CID, EOM, REMAIN, rxMessage = receiveUDP(ACK, EOM, REMAIN, soc)
-        print(rxMessage)
-        if not EOM:
-            if not ACK:
-                message = "Send again"
-                sendUDP(ACK, CID, EOM, REMAIN, message, address, port, soc)
-            else:
-                message = reverseWords(rxMessage)
-                sendUDP(ACK, CID, EOM, REMAIN, message, address, port, soc)
+        sendUDP(ACK, CID, EOM, message, address, port, soc)
+        while not EOM:
+            ACK, CID, EOM, REMAIN, rxMessage = receiveUDP(soc)
+            print(rxMessage)
+            if not EOM:
+                if not ACK:
+                    message = "Send again"
+                    sendUDP(ACK, CID, EOM, message, address, port, soc)
+                else:
+                    message = reverseWords(rxMessage)
+                    sendUDP(ACK, CID, EOM, message, address, port, soc)
 
-    return
+        return
+    except TimeoutError:
+        print("could not connect to UDP server")
+        sys.exit(USAGE)
 
 
-def receiveUDP(ACK, EOM, REMAIN, soc):
+def receiveUDP(soc: socket) -> tuple[bool, str, bool, int, str]:
+    """
+    receives UDP messages and constructs the complete message from parts
+    :param soc: server socket
+    :return: tuple of ACK, CID, EOM, REMAIN and the constructed message
+    """
     whole_message = ""
     while 1:
         rxBuf = soc.recv(BUF_LEN)
@@ -140,7 +175,19 @@ def receiveUDP(ACK, EOM, REMAIN, soc):
     return ACK, CID, EOM, REMAIN, whole_message
 
 
-def sendUDP(ACK, CID, EOM, REMAIN, message, address, port, soc):
+def sendUDP(ACK: bool, CID: str, EOM: bool, message: str, address: str, port: int, soc: socket) -> None:
+    """
+    sends UDP messages to the server constructed from the data. If message is longer than MSG_LEN it will be sent
+    in multiple parts. Parity bits are added to the messages, and they are encoded with enc_keys
+
+    :param ACK: Whether parity of the last message was correct
+    :param CID: Unique client identifier provided by the server
+    :param EOM: boolean to keep track of the end of communication
+    :param message: complete message to send to the server
+    :param address: server address
+    :param port: server port
+    :param soc: server socket
+    """
     print(message)
     messages = splitMessage(message)
     CID = str.encode(CID)
@@ -157,7 +204,12 @@ def sendUDP(ACK, CID, EOM, REMAIN, message, address, port, soc):
         soc.sendto(data, (address, port))
 
 
-def splitMessage(message):
+def splitMessage(message: str) -> list[str]:
+    """
+    if length of message is over MSG_LEN, splits the message into multiple parts
+    :param message: complete message
+    :return: list of message pieces of length MSG_LEN
+    """
     messages = []
     if len(message) > MSG_LEN:
         pieceAmount = math.ceil(len(message) / MSG_LEN)
@@ -170,15 +222,27 @@ def splitMessage(message):
     return messages
 
 
-def reverseWords(string):
-    words = string.split()[::-1]
+def reverseWords(message: str) -> str:
+    """
+    reverses words of the message
+    e.g. one two three becomes three two one
+
+    :param message: message to reverse
+    :return: message with words in reverse order
+    """
+    words = message.split()[::-1]
     ret = []
     for i in words:
         ret.append(i)
     return " ".join(ret)
 
 
-def addParity(message):
+def addParity(message: str) -> str:
+    """
+    adds Even parity to each character of the message
+    :param message: message to add parity to
+    :return: message with parity
+    """
     parityMsg = ""
     for char in message:
         binChar = bin(ord(char))[2:]
@@ -191,7 +255,12 @@ def addParity(message):
     return parityMsg
 
 
-def checkParity(message):
+def checkParity(message: str) -> bool:
+    """
+    checks if even parity of the message is correct
+    :param message: message to check
+    :return: true if parity is correct
+    """
     for char in message:
         binChar = bin(ord(char))[2:]
         ones = binChar.count('1')
@@ -199,7 +268,13 @@ def checkParity(message):
             return False
     return True
 
-def removeParity(message):
+
+def removeParity(message: str) -> str:
+    """
+    removes parity bits from the message
+    :param message:
+    :return: message with parity bits removed
+    """
     cleanMessage = ""
     for char in message:
         bin_char = bin(ord(char))[2:]
@@ -208,29 +283,29 @@ def removeParity(message):
         cleanMessage += chr(bin_char)
     return cleanMessage
 
+
 def main():
-    USAGE = 'usage: %s <server address> <server port> <message>' % sys.argv[0]
+    server_address = "195.148.20.105"
+    server_tcp_port = 10000
+    message = "HELLO ENC MUL PAR\r\n"
+
     try:
         # Get the server address, port and message from command line arguments
-
-        # server_address = str(sys.argv[1])
-        # server_tcpport = int(sys.argv[2])
-        # message = str(sys.argv[3])
-        server_address = "195.148.20.105"
-        server_tcpport = 10000
-        message = "HELLO ENC MUL PAR\r\n"
         gen_enc_keys(KEY_AMOUNT)
         for i in range(0, KEY_AMOUNT):
             message = message + f"{enc_keys[i]}\r\n"
         message = message + ".\r\n"
+
+        server_address = str(sys.argv[1])
+        server_tcp_port = int(sys.argv[2])
+        message = str(sys.argv[3])
     except IndexError:
-        print("Index Error")
+        print("No arguments given, starting with defaults")
     except ValueError:
-        print("Value Error")
-        # Print usage instructions and exit if we didn't get proper arguments
+        print("Incorrect arguments, please read the usage")
         sys.exit(USAGE)
 
-    send_and_receive_tcp(server_address, server_tcpport, message)
+    send_and_receive_tcp(server_address, server_tcp_port, message)
 
 
 if __name__ == '__main__':
